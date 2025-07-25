1a909ff16e890c971faacf0d571d6960d43c3218
Daniel <danolox@gmail.com>
2025-07-25 15:18:41 +0000

    pequeños cambios

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey, Enum, CheckConstraint, PrimaryKeyConstraint, DateTime # "ForeignKey", "Enum", "CheckConstraint", "PrimaryKeyConstraint" imported
-from sqlalchemy.orm import Mapped, mapped_column, relationship # "relationship" imported
+# "ForeignKey", "Enum", "CheckConstraint", "PrimaryKeyConstraint" imported
+from sqlalchemy import String, Boolean, ForeignKey, Enum, CheckConstraint, PrimaryKeyConstraint, DateTime
+# "relationship" imported
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, timezone
 import enum
 from sqlalchemy.dialects.postgresql import ENUM as PGEnum
@@ -30,7 +32,6 @@ TO-DOs:
 """
 
 
-
 ############################################
 ###########         User         ###########
 ############################################
@@ -54,20 +55,27 @@ TO-DO's:
 
 [x] Create serialization
 """
+
+
 class User(db.Model):
     __tablename__ = "user"
 
     ### ATRIBUTES ###
 
-    id:         Mapped[int]  = mapped_column(              primary_key=True)
-    username:   Mapped[str]  = mapped_column( String(20),  unique=True,       nullable=False)
-    first_name: Mapped[str]  = mapped_column( String(40),                     nullable=False)
-    last_name:  Mapped[str]  = mapped_column( String(40),                     nullable=False)
-    email:      Mapped[str]  = mapped_column( String(60),  unique=True,       nullable=False)
+    id:         Mapped[int] = mapped_column(primary_key=True)
+    username:   Mapped[str] = mapped_column(
+        String(20),  unique=True,       nullable=False)
+    first_name: Mapped[str] = mapped_column(
+        String(40),                     nullable=False)
+    last_name:  Mapped[str] = mapped_column(
+        String(40),                     nullable=False)
+    email:      Mapped[str] = mapped_column(
+        String(60),  unique=True,       nullable=False)
 
-    password:   Mapped[str]  = mapped_column( String(40),                     nullable=False)
-    is_active:  Mapped[bool] = mapped_column( Boolean(),   default=True,      nullable=False)
-
+    password:   Mapped[str] = mapped_column(
+        String(40),                     nullable=False)
+    is_active:  Mapped[bool] = mapped_column(
+        Boolean(),   default=True,      nullable=False)
 
     ### RELATIONS ###
 
@@ -76,27 +84,26 @@ class User(db.Model):
         "Follow",
         foreign_keys="Follow.user_from_id",
         back_populates="follower"
-        )
-    
+    )
+
     # with Follower --> Users following this user
     followers: Mapped[list["Follow"]] = relationship(
         "Follow",
         foreign_keys="Follow.user_to_id",
         back_populates="followed"
-        )
-    
+    )
+
     # with Comment --> Comments made by this user
     comments: Mapped[list["Comment"]] = relationship(
         "Comment",
         back_populates="author"
-        )
+    )
 
     # with Post --> Posts made by this user
     user_posts: Mapped[list["Post"]] = relationship(
         "Post",
         back_populates="post_author"
-        )
-
+    )
 
     ### SERIALIZATION ###
 
@@ -108,13 +115,12 @@ class User(db.Model):
             "username":    self.username,
             "first_name":  self.first_name,
             "last_name":   self.last_name,
-            "following":   [follow.serialize() for follow in self.following],
+            "following":   [followed.serialize()   for followed   in self.following],
             "followers":   [follower.serialize() for follower in self.followers],
-            "comments":    [comment.serialize() for comment in self.comments],
-            "posts":       [post.serialize() for post in self.posts]
+            "comments":    [comment.serialize()  for comment  in self.comments],
+            "posts":       [post.serialize()     for post     in self.user_posts]
             # do not serialize the password, its a security breach !!!
         }
-
 
 
 ############################################
@@ -138,41 +144,45 @@ TO-DO's:
 
 [x] Create serialization
 """
+
+
 class Follow(db.Model):
     __tablename__ = "follow"
-
 
     ### ATRIBUTES ###
 
     # id: Mapped[int] = mapped_column(primary_key=True)
-    user_from_id:  Mapped[int]  = mapped_column(  ForeignKey("user.id"),   primary_key=True)
-    user_to_id:    Mapped[int]  = mapped_column(  ForeignKey("user.id"),   primary_key=True)
+    user_from_id:  Mapped[int] = mapped_column(
+        ForeignKey("user.id"),   primary_key=True)
+    user_to_id:    Mapped[int] = mapped_column(
+        ForeignKey("user.id"),   primary_key=True)
 
-    created_at:    Mapped[datetime] = mapped_column( DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))   # default=datetime.now
+    created_at:    Mapped[datetime] = mapped_column(DateTime(
+        # default=datetime.now
+        timezone=True), default=lambda: datetime.now(timezone.utc))
 
-    ## COMPOSITE PRIMARY KEY
+    # COMPOSITE PRIMARY KEY
     __table_args__ = (
         PrimaryKeyConstraint("user_from_id", "user_to_id"),
-        CheckConstraint("user_from_id != user_to_id", name="check_self_follow"),
+        CheckConstraint("user_from_id != user_to_id",
+                        name="check_self_follow"),
     )
 
-
     ### RELATIONS ###
-    
+
     # with User -> from "user_from_id" --> User who is following (follower)
     follower: Mapped["User"] = relationship(
         "User",
         foreign_keys=[user_from_id],
         back_populates="following"
-        )
+    )
 
     # with User -> from "user_to_id" --> User being followed (followed)
     followed: Mapped["User"] = relationship(
         "User",
         foreign_keys=[user_to_id],
         back_populates="followers"
-        )
-    
+    )
 
     ### SERIALIZATION ###
 
@@ -182,7 +192,6 @@ class Follow(db.Model):
             "user_to_id":    self.user_to_id,
             "created_at":    self.created_at.isoformat() if self.created_at else None
         }
-
 
 
 ############################################
@@ -206,18 +215,22 @@ TO-DO's:
 
 [x] Create serialization
 """
+
+
 class Comment(db.Model):
     __tablename__ = "comment"
 
     ### ATRIBUTES ###
 
-    id:           Mapped[int]     = mapped_column(               primary_key=True)
-    comment_text: Mapped[str]     = mapped_column( String(500),                         nullable=False)
-    author_id:    Mapped[int]     = mapped_column(               ForeignKey("user.id"))
-    post_id:      Mapped[int]     = mapped_column(               ForeignKey("post.id"))
+    id:           Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(
+        String(500),                         nullable=False)
+    author_id:    Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id:      Mapped[int] = mapped_column(ForeignKey("post.id"))
 
-    created_at:  Mapped[datetime] = mapped_column( DateTime(timezone=True),   default=lambda: datetime.now(timezone.utc))   # default=datetime.now
-
+    created_at:  Mapped[datetime] = mapped_column(DateTime(
+        # default=datetime.now
+        timezone=True),   default=lambda: datetime.now(timezone.utc))
 
     ### RELATIONS ###
 
@@ -225,17 +238,16 @@ class Comment(db.Model):
     author: Mapped["User"] = relationship(
         "User",
         back_populates="comments"
-        )
+    )
 
     # with Post --> Post this comment belongs to
     post: Mapped["Post"] = relationship(
         "Post",
         back_populates="comments"
-        )
-
+    )
 
     ### SERIALIZATION ###
-    
+
     def serialize(self):
         return {
             "id":            self.id,
@@ -267,15 +279,20 @@ TO-DO's:
 
 [x] Create serialization
 """
+
+
 class Post(db.Model):
     __tablename__ = "post"
 
     ### ATRIBUTES ###
 
-    id:        Mapped[int]  = mapped_column(  primary_key=True)
-    user_id:   Mapped[int]  = mapped_column(  ForeignKey("user.id"),   nullable=False)
+    id:        Mapped[int] = mapped_column(primary_key=True)
+    user_id:   Mapped[int] = mapped_column(
+        ForeignKey("user.id"),   nullable=False)
 
-    created_at: Mapped[datetime] = mapped_column( DateTime(timezone=True),   default=lambda: datetime.now(timezone.utc))   # default=datetime.now
+    created_at: Mapped[datetime] = mapped_column(DateTime(
+        # default=datetime.now
+        timezone=True),   default=lambda: datetime.now(timezone.utc))
 
     ### RELATIONS ###
 
@@ -283,28 +300,26 @@ class Post(db.Model):
     post_author: Mapped["User"] = relationship(
         "User",
         back_populates="user_posts"
-        )
+    )
 
     # with Comment --> Comments on this post
     comments: Mapped[list["Comment"]] = relationship(
         "Comment",
         back_populates="post"
-        )
+    )
 
     # with Media --> Media attached to this post
     media: Mapped[list["Media"]] = relationship(
         "Media",
         back_populates="post"
-        )
-
-
+    )
 
     def serialize(self):
         return {
             "id":          self.id,
             "user_id":     self.user_id,
             "created_at":  self.created_at.isoformat() if self.created_at else None,
-            "media":       [m.serialize() for m in self.media],
+            "media":       [media.serialize()   for media   in self.media],
             "comments":    [comment.serialize() for comment in self.comments]
         }
 
@@ -334,32 +349,34 @@ TO-DO's:
 """
 Con el tipo "mediatype" con enum (enumaración) da error, no sé cómo arreglarlo, así que lo quito para que no salte error al hager el "diagram.png"
 """
-### Media type for the "enum" data type
+# Media type for the "enum" data type
 # class MediaType(str, Enum):  # -------> Apparently wrong syntax
 # class MediaType(enum.Enum):
-    # IMAGE = "image"
-    # VIDEO = "video"
+# IMAGE = "image"
+# VIDEO = "video"
 
 ############################################################
+
+
 class Media(db.Model):
     __tablename__ = "media"
 
     ### ATRIBUTES ###
 
-    id:       Mapped[int]        = mapped_column(                   primary_key=True)
+    id:       Mapped[int] = mapped_column(primary_key=True)
     # type:     Mapped[MediaType]  = mapped_column(  PGEnum(MediaType, name="mediatype", create_type=True),  nullable=False)  ### -------> DA ERROR
-    url:      Mapped[str]        = mapped_column(  String(255),                                           nullable=False)
-    post_id:  Mapped[int]        = mapped_column(                   ForeignKey("post.id"),                nullable=False)
-
+    url:      Mapped[str] = mapped_column(
+        String(255),                                           nullable=False)
+    post_id:  Mapped[int] = mapped_column(
+        ForeignKey("post.id"),                nullable=False)
 
     ### RELATIONS ###
 
     # with Post --> Post this media belongs to
     post: Mapped["Post"] = relationship(
         "Post",
-          back_populates="media"
-          )
-
+        back_populates="media"
+    )
 
     ### SERIALIZATION ###
 
@@ -370,7 +387,3 @@ class Media(db.Model):
             "url":      self.url,
             "post_id":  self.post_id,
         }
-
-
-
-
